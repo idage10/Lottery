cde4862693c71d5def3ffc1b76d8ec40c0520582
idage10 <103930607+idage10@users.noreply.github.com>
2024-01-18 03:24:08 +0200

    Add more Lottery design

---
+++ src/Main.js
@@ -0,0 +1,8 @@
+import { Lottery } from './Lottery';
+
+// A functional component to display the Main Page
+export const Main = () => {
+    return (
+      <Lottery />
+    );
+  };
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './Main.css';
-import { Lottery } from './Lottery';
+import { Main } from './Main';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Lottery />
+    <Main />
   </React.StrictMode>
 );
 
