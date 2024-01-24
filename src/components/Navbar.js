eeb4cb6e276a9489c98c0d4c2eba70abd88791dd
idage10 <103930607+idage10@users.noreply.github.com>
2024-01-24 18:39:52 +0200

    npm audit fix --force
    Update packages and set menuOpen and toggled to false when changing resolution from large screen to mobile screen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import useMediaQuery from '@mui/material/useMediaQuery';
 import "./Navbar.css";
 
 export const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
     const [toggled, setToggled] = useState(false);
+
+    // use the hook to check the screen size
+    const isMobileScreen = useMediaQuery("(max-width: 480px)");
+
+    React.useEffect(() => {
+        if (isMobileScreen) {
+            // set menuOpen and toggled to false if the screen is mobile screen
+            setMenuOpen(false);            
+            setToggled(false);
+        }
+    }, [isMobileScreen]); // update the effect when the screen size changes
     
     return (
         <nav>
